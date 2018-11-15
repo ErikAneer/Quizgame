5028cf73e0447e8d3ae477ecac5a08903f8690f5
David Sodergren <david.sodergren94@gmail.com>
2018-11-15 10:30:03 +0100

    First commit by Dave

---
+++ ServerProtocol.java
@@ -0,0 +1,35 @@
+public class ServerProtocol {
+
+
+    public enum EnumState{
+        waiting, questionOne
+    }
+
+    private static EnumState state = EnumState.waiting;
+    private String returnMessage;
+
+    public String handleInput(String answer){
+
+        if(state == EnumState.waiting && answer == null){
+            state = EnumState.questionOne;
+            returnMessage = "Varmt välkommen! För att starta, skriv: q!";
+
+        }
+        else if(state == EnumState.questionOne){
+            if(answer.equalsIgnoreCase("q")) {
+                returnMessage = "Vilken färg gillar Erik? grön, röd, svart, gul";
+            }
+            else if(answer.equalsIgnoreCase("grön")){
+                state = EnumState.waiting;
+                returnMessage = "Rätt svar";
+            }
+            else
+                returnMessage = "Fel svar";
+        }
+            return returnMessage;
+    }
+
+
+
+
+}
