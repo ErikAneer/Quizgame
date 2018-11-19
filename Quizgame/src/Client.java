0349becb17b6e60d12ae52b30a947e349997d1ba
David Sodergren <david.sodergren94@gmail.com>
2018-11-19 12:57:33 +0100

    First commit

--- Quizgame/src/Client.java
+++ Quizgame/src/Client.java
@@ -1,45 +1,43 @@
-
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-    
-             InetAddress ipAdress = InetAddress.getLocalHost();
 
-        public Client() throws UnknownHostException{
-            
-       
-             try (Socket socket = new Socket(ipAdress, 55555);
+    InetAddress ipAdress = InetAddress.getLocalHost();
 
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ) {
+    public Client() throws UnknownHostException{
 
+
+        try (Socket socket = new Socket(ipAdress, 55555);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+        ) {
             Scanner scan = new Scanner(System.in);
-
             String messageFromClient;
             String messageFromServer;
 
-            while(true){
 
-                messageFromServer = reader.readLine();
+            while(true){
+                messageFromServer = (String) objectInputStream.readObject();
                 System.out.println(messageFromServer);
+                if(messageFromServer.equalsIgnoreCase("Väntar på kategori från motståndare")){
+                    messageFromClient = "";
+                    objectOutputStream.writeObject(messageFromClient);
+                    messageFromServer = (String) objectInputStream.readObject();
+                    System.out.println(messageFromServer);
+                }
 
                 messageFromClient = scan.nextLine();
-                writer.println(messageFromClient);
-
+                objectOutputStream.writeObject(messageFromClient);
             }
 
         }catch(IOException e){
             System.out.println("Error: " + e.getMessage());
+        }catch (ClassNotFoundException e){
+            e.getMessage();
         }
 
 
